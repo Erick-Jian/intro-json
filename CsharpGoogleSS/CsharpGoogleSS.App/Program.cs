6960c1533d58cbfaf9182d1f9db2beec74be6464
Erick-Jian <jiane@stpaulsschool.org.uk>
2021-04-09 21:56:58 +0100

    1st
    Setting up connection between Spreadsheet & VS
    more to come

---
+++ CsharpGoogleSS/CsharpGoogleSS.App/Program.cs
@@ -0,0 +1,83 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;   
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace CsharpGoogleSS.App
+{
+    #region Google Spreadsheet API:
+    ///     Client ID: 147244942006-lidco6hc26nm2pe4af5nb1rn1131u722.apps.googleusercontent.com
+    ///     Client Secret: LqYsz0eNNsENarwmZeQrSqmf
+    ///     .Net setup: https://developers.google.com/sheets/api/quickstart/dotnet
+    ///     Download Clinet Config: data:attachment/json;charset=utf-8,%7B%22installed%22%3A%7B%22client_id%22%3A%22147244942006-lidco6hc26nm2pe4af5nb1rn1131u722.apps.googleusercontent.com%22%2C%22project_id%22%3A%22quickstart-1616964666391%22%2C%22auth_uri%22%3A%22https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth%22%2C%22token_uri%22%3A%22https%3A%2F%2Foauth2.googleapis.com%2Ftoken%22%2C%22auth_provider_x509_cert_url%22%3A%22https%3A%2F%2Fwww.googleapis.com%2Foauth2%2Fv1%2Fcerts%22%2C%22client_secret%22%3A%22LqYsz0eNNsENarwmZeQrSqmf%22%2C%22redirect_uris%22%3A%5B%22urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob%22%2C%22http%3A%2F%2Flocalhost%22%5D%7D%7D
+    ///     
+    ///     Name: Quickstart
+    ///     Type: Desktop App
+
+    #endregion
+
+    #region Connecting Spreadsheet with Visual Studio C#   &   fetching data (with credentials.j)
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync( GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";  // the series AFTER  spreadsheets/d/
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            Console.Read();
+        }
+    }
+    #endregion
+}
--- IntroductionToJsonFull/IntroductionToJsonFull.App/Program.cs
+++ IntroductionToJsonFull/IntroductionToJsonFull.App/Program.cs
@@ -1,11 +1,17 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;   // WebClient
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace IntroductionToJsonFull.App
+    /// serialize - storing stuff as text (Lists, Objects) -- language independent
+    /// build allows the program to search for new packages, and "inclnude" them in program
 {
     class Program
     {
@@ -18,26 +24,49 @@ namespace IntroductionToJsonFull.App
             string json = @"{""key1"":""value1"",""key2"":""value2""}";
 
             IDictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            // An Interface
 
             foreach (string k in values.Keys)
                 Console.WriteLine($"{k}: {values[k]}");
 
 
             // e.g. https://www.newtonsoft.com/json/help/html/DeserializeObject.htm 
+            // json string
             string json2 = @"{
             'Email': 'james@example.com',
             'Active': true,
             'CreatedDate': '2021-01-20T00:00:00Z',
             'Roles': [
                 'User',
-                'Admin'
-            ]
-            }";
+                'Admin'     ]   }";
 
-            Account account = JsonConvert.DeserializeObject<Account>(json2);
+            Account account = JsonConvert.DeserializeObject<Account>(json2);    // takes the text match the field names
 
             Console.WriteLine(account.Email);
 
+            Account a2 = new Account("a@b.com", false, DateTime.UtcNow, new List<string> { "user" });
+            string j3 = JsonConvert.SerializeObject(a2);
+            Console.WriteLine(j3);
+
+
+            string jsonDownload;
+
+            // IDisposible: create a Dispose "method"
+            // WebClient: browser, fetch data.
+            using (var wc = new WebClient())    // creates the connetion & close it
+            {
+                jsonDownload = wc.DownloadString("https://blockchain.info/latestblock");
+                    // CONNECT, DOWNLOAD the text in the url
+            }
+            // DISCONNECT the linkage to the URL
+
+            dynamic j = JArray.Parse(jsonDownload);
+
+            dynamic l = j[1];
+
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+                Console.WriteLine("Press [esc] to exit");
         }
 
         public class Account
@@ -59,6 +88,186 @@ namespace IntroductionToJsonFull.App
                 _created = c;
                 _roles = r;
             }
+        }
+
+        public class BlocksBTC
+        {
+            string _SHA256hash;
+            int _time;              // in Unix Format: https://en.wikipedia.org/wiki/Unix_time
+            int _BlockIndex;
+            int _height;
+            Array[] _txIndexes;
+
+            public BlocksBTC(string HASH, int Unix_TIME, int INDEX, int HEIGHT, Array[] TXIND)
+            {
+                _SHA256hash = HASH;
+                _time = Unix_TIME;      // Unix epoch is 00:00:00 UTC on 1 January 1970; number of SECONDs
+                _BlockIndex = INDEX;
+                _height = HEIGHT;
+                _txIndexes = TXIND;
+            }
+
+            public string SHA256Hash { get => _SHA256hash; set => _SHA256hash = value; }
+            public int Time { get => _time; set => _time = value; }
+            public int BlockIndex { get => _BlockIndex; set => _BlockIndex = value; }
+            public int Height { get => _height; set => _height = value; }
+            public Array[] TxIndexes { get => _txIndexes; set => _txIndexes = value; }
+
+        }
+    }
+}
+
+
+namespace Week_5_File_PREP.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q1();
+            Q2();
+            Q3();
+
+            Console.ReadKey();
+        }
+
+        static void Q1()
+        {
+            // Input String
+            string content = "Hello File";          // Could be replaced by Console.WriteLine & ReadLine
+
+            // create new file
+            string filename = "sentences_original.txt";
+            File.WriteAllText(filename, content);   // Create an "intermediate" file and write the contents of "content" to it
+
+            // grab the info: both the content and the path
+            StreamReader filereader = File.OpenText(filename);          // stores the content in the file in "filereader"
+            string docpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);  // desired output location
+
+            // combine
+            string docinfo = Path.Combine(docpath, "sentences.txt");     /* combine path and O/P file name
+                                                                                         to form the location */
+            bool exists = File.Exists(filename);    // presence check
+
+            // add text after the text that already exists
+            if (exists)
+            {
+                Console.WriteLine("Already Exists");
+                Console.WriteLine("Do you want to write sth else ?");
+
+                // using provides a localized "function" - wrapping for output file so don'e need to close it
+                using (StreamWriter filewriter = File.AppendText(docinfo))     // alternative: new StreamWriter(docinfo, true)
+                {
+                    string line;
+                    while (true)
+                    {
+                        line = filereader.ReadLine();       // reads the text in the file stored in "filereader" line by line
+                        if (line == null)
+                        { break; }                          // stops when all lines has been read and written
+                        filewriter.WriteLine(line);
+                    }
+                    // close the Streamreader
+                    filewriter.Close();
+                }
+                // close the Streamwriter
+                filereader.Close();
+            }
+            // creating new file / overwriting nothing
+            else
+            {
+                using (StreamWriter filewriter = File.CreateText(docinfo))      // alternative: new StreamWriter(docinfo)
+                {
+                    // doing the same thing
+                    string line = string.Empty;
+                    while (line != null)
+                    {
+                        line = filereader.ReadLine();
+                        if (line != null)
+                        {
+                            filewriter.WriteLine(line);
+                        }
+                    }
+                    // close both
+                    filewriter.Close();
+                }
+                filereader.Close();
+            }
+        }
+
+        static void Q2()
+        {
+            int counts = 4;
+            int max = 10;
+
+            // Random Number Generator setup
+            Random rand = new Random();         // variable type: random number
+
+            // Create the location of the output file
+            string doc_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string doc_loc = Path.Combine(doc_path, "Q2_Main.txt");
+
+            if (!File.Exists(doc_loc))
+            {
+                using (StreamWriter SW = File.CreateText(doc_loc))
+                {
+                    for (int i = 0; i < counts; i++)
+                    {
+                        SW.WriteLine(rand.Next(1, max + 1));
+                    }
+                    SW.Close();
+                }
+            }
+
+            // 2nd file - sorting   
+            string doc_path2 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string doc_loc2 = Path.Combine(doc_path2, "Q2_Sorted.txt");     // generate full path
+
+            // placing the values in list
+
+            List<string> ListofStrings = File.ReadAllLines(doc_loc).ToList();
+
+            foreach (string n in ListofStrings)
+            {
+                Console.WriteLine(n);
+            }
+            List<int> ListofIntegers = ListofStrings.Select(x => int.Parse(x)).ToList();  // Stack Overflow
+
+            int SumOfList = 0;
+            foreach (int INTEGERS in ListofIntegers)
+            {
+                SumOfList += INTEGERS;      // compare using system method & manual method
+            }
+            Debug.Assert(SumOfList == ListofIntegers.Sum());    // type & value check
+
+            //List<int> ListofIntegers = ListofStrings.Select(x => new int() { SomeValue = x.SomeValue }).ToList();
+
+            /* Sorting strategy: bubble sort, the smaller value goes into another list, where this process is repeated until 
+              foreach int in list Debug.Assert(PREVIOUS_VALUE < NEXT_VALUE) */
+        }
+
+        static void Q3()
+        {
+            // get the path of the file
+            string filename = "stations.txt";
+            string PATH = Path.GetFullPath(filename);
+            List<string> Alphabet = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            if (!String.IsNullOrEmpty(PATH))    // Stack Overflow - LinQ Query method
+            {
+                var query = File.ReadLines(PATH).Where(line => !String.IsNullOrEmpty(line)).GroupBy(line => line.First());
+                /*IDs = query.Select(a => a.ID).ToList();
+                List<string> IDs = (from c in doc.Root.Elements("a").Elements("b") select c.Element("val").Value).ToList()*/
+                // this structure will be used to get values from linQ query to a string List
+
+                foreach (string l in Alphabet)
+                {
+                    if (!IDs.Contains(l))
+                    {
+                        Console.WriteLine(l);
+                    }
+                }
+            }
+
         }
     }
 }
